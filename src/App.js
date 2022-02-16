fb423db86401489d20cdad16df0133134682ab6a
Lee-Mina <95553437+Lee-Mina@users.noreply.github.com>
2022-02-16 12:59:36 +0900

    2022.02.16

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
 import React, { useState, useEffect, Component } from "react";
 import logo from "./logo.svg";
 import styled from "./App.css";
-// import styled from "styled-components";
-
-//스타일
-// const 블로그 = styled.div`
-//   width: 678.4px;
-//   height: 479.67px;
-
-//   box-shdow: 0 0 0 1px rgb(255 255 255);
-//   overflow-wrap: anywhere; ;
-// `;
-// const Publish = styled.div`
-//   backgrounds-color: rgb(59 73 233);
-//   border-color: white;
-//   display: inline-block;
-//   cursor: pointer;
-//   overflow-wrap: nomal;
-//   color: rgb(255 255 255);
-// `;
-
-// const WriteHere = styled.div`;
-//   resize: none;
-// `;
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function App() {
+  const [blogContent, setBlogContent] = useState({
+    title: "",
+    content: "",
+  });
+
   return (
     <div className="블로그">
       <Header />
       <div className="헤더제외모든디브">
         <div className="가로두개">
           <div className="topAndMiddle">
-            <Top />
-            <Middle />
+            <Top blogContent={blogContent} setBlogContent={setBlogContent} />
+            <MiddleTop
+              blogContent={blogContent}
+              setBlogContent={setBlogContent}
+            />
+            {/* <Middle /> */}
           </div>
           <Side />
         </div>
@@ -57,7 +46,14 @@ function Header() {
   );
 }
 
-function Top() {
+function Top(props) {
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    props.setBlogContent({
+      ...props.blogContent,
+      [name]: value,
+    });
+  };
   return (
     <div className="제목과이미지">
       <button className="addImg">Add a cover image</button>
@@ -65,17 +61,40 @@ function Top() {
         Type="text"
         className="제목"
         placeholder="New post title here..."
+        onChange={getValue}
+        name="title"
       ></textarea>
     </div>
   );
 }
 
-function Middle() {
+function MiddleTop(props) {
   return (
-    <textarea
-      className="내용"
-      placeholder="Write your post content here..."
-    ></textarea>
+    <div className="내용">
+      <CKEditor
+        editor={ClassicEditor}
+        data="<textarea >Write your post content here...</textarea>"
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+          props.setBlogContent({
+            ...props.blogContent,
+            content: data,
+          });
+          console.log(props.blogContent);
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+      />
+    </div>
   );
 }
 
